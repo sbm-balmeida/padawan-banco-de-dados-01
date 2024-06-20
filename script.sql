ceda78145fa93fc8bbf66b1642b1b19e5803b8e9
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-20 14:15:21 -0300

    Desafio: Incluindo mais dois vendedores

--- script.sql
+++ script.sql
@@ -83,3 +83,19 @@ PERCENTUAL_COMISSAO) VALUES(
 '00233', 
 'João Geraldo da Fonseca', 
 0.10);
+
+INSERT INTO tabela_de_vendedores(
+MATRICULA, 
+NOME, 
+PERCENTUAL_COMISSAO) VALUES (
+'00235',
+'Márcio Almeida Silva',
+0.08);
+
+INSERT INTO tabela_de_vendedores(
+MATRICULA, 
+NOME, 
+PERCENTUAL_COMISSAO) VALUES (
+'00236',
+'Cláudia Morais',
+0.08);
