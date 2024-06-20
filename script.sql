f051604cbd3a60ca344c88876726e2c5dd93e8ed
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-06-20 13:56:51 -0300

    Inserindo registros na tabela

--- script.sql
+++ script.sql
@@ -17,13 +17,32 @@ IDADE SMALLINT,
 SEXO VARCHAR(1),
 LIMITE_CREDITO FLOAT,
 VOLUME_COMPRA FLOAT,
-PRIMEIRA_COMPRA BIT(1))
+PRIMEIRA_COMPRA BIT(1));
+
+USE Sucos; #comando para selecionar um banco
+
+DROP TABLE tbcliente3; #comando para apagar uma tabela
 
 #Desafio Aula-03
 CREATE TABLE TABELA_DE_VENDEDORES (
 NOME VARCHAR(100), 
 PERCENTUAL_COMISSAO FLOAT);
 
-USE Sucos; #comando para selecionar um banco
+#Aula-04
+USE Sucos;
 
-DROP TABLE tbcliente3; #comando para apagar uma tabela
+INSERT INTO tbproduto ( #comando para inserir dados na tabela
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PRECO_LISTA) VALUES (
+'1040107', 
+'Light - 350 ml - Melancia',
+'Lata', 
+'350 ml', 
+'Melancia', 
+4.56);
+
+SELECT * FROM tbproduto; #comando para selecionar tudo da tabela tbproduto
